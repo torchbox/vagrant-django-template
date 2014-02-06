c2d1387a8a2e2e06e7f186f3071e733f101853a4
Matt Westcott <matt@west.co.tt>
2014-02-06 15:16:58 +0000

    don't specify exact versions of libjpeg / libtiff, to avoid apt package conflicts

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -32,7 +32,7 @@ wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | pyth
 # Dependencies for image processing with Pillow (drop-in replacement for PIL)
 # supporting: jpeg, tiff, png, freetype, littlecms
 # (pip install pillow to get pillow itself, it is not in requirements.txt)
-apt-get install -y libjpeg62-dev libtiff4-dev zlib1g-dev libfreetype6-dev liblcms2-dev
+apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
 # Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
 apt-get install -y git
 
