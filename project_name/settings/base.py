11fa2414294a470c2e6ec60348a9acba7d133860
Matt Westcott <matt@west.co.tt>
2016-01-01 18:16:44 +0000

    Upgrade Django Debug Toolbar

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -60,8 +60,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = '{{ project_name }}.urls'
@@ -170,11 +168,6 @@ EMAIL_SUBJECT_PREFIX = '[{{ project_name }}] '
 
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 
-
-# django-debug-toolbar settings
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
 
 # django-compressor settings
 COMPRESS_PRECOMPILERS = (
