3e7e0aa10edb19f69a8a51df22fed582333bb041
Matt Westcott <matt@west.co.tt>
2016-01-02 02:44:42 +0000

    clean up postgres setup

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '{{ project_name }}',
-        'USER': 'postgres',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '',  # Set to empty string for localhost.
         'PORT': '',  # Set to empty string for default.
